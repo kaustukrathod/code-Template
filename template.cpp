e894ce325ed6531085447de85e70116e0b0a9088
kaustukrathod <kaustukrathod@outlook.com>
2017-07-12 16:11:17 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end() 
+#define tr(c,i) for(auto i = (c).begin(); i != (c).end(); i++) 
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define make0(a) memset(a,0,sizeof(a))
+#define make1(a) memset(a,1,sizeof(a))
+#define fast_cin() ios_base::sync_with_stdio(0) 
+const int mod = 1e9+7;
+using namespace std;
+
+int main()
+{
+    fast_cin();
+    return 0;
+}
